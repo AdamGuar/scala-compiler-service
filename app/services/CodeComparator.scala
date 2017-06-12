4dfd82a9fc6cc2c5995585966d5e2d09e189bf0d
Michał Suski <uzikus1@gmail.com>
2017-06-12 22:00:58 +0200

    Similarity code cleanup

--- app/services/CodeComparator.scala
+++ app/services/CodeComparator.scala
@@ -8,13 +8,12 @@ import java.util
 import info.debatty.java.stringsimilarity.JaroWinkler
 import java.lang.System
 import play.api.libs.json._
-import java.util.ArrayList
 import scala.collection.mutable.ListBuffer
 
 /**
   * Class takes a codeID and compare file with given id with all other codes
   *
-  * @author Michał Suski i Karol SKó®a
+  * @author Michał Suski i Dawid Zych
   * @param codeID: id of code to compare
   *
   */
@@ -30,19 +29,13 @@ class CodeComparator(id: String) {
   )
 }
   
-  
   private var codeID = id
   private val folder = new File(System.getProperty("user.dir")+"/code") 
   
-  def compare(): JsValue/*: Map[String, Float] */= {
-    
-    
-   // var result = "";
-    
+  def compare(): JsValue = {
+
     var simList = new ListBuffer[Sim]()
-    
     var resultArrJson = new JsArray();
-   
     val fileNames = folder.listFiles()
     val nl = new JaroWinkler()
     val initIdList = Files.readAllLines(Paths.get(folder+"/"+id),StandardCharsets.UTF_8).toArray()
@@ -51,30 +44,14 @@ class CodeComparator(id: String) {
     for (line <- initIdList) {
       initIdString += line
     }
-    
-    for (fileName <- fileNames) {
-      
+    for (fileName <- fileNames) {  
         var fileString = ""
         val fileStringList = Files.readAllLines(fileName.toPath(),StandardCharsets.UTF_8).toArray()
-        
         for (line <- fileStringList) {
            fileString += line
-        }
-        
+        }    
         simList += (new Sim(fileName.getName(), nl.similarity(initIdString, fileString)))
-        
-        //resultArrJson.append(Json.obj("ID" -> fileName.getName(), "Val" -> nl.similarity(initIdString, fileString)))
-
-      //  resultArr.add(new Sim(fileName.getName(), nl.similarity(initIdString, fileString)))
-        
-       // result += "Id: " + fileName.getName() + " Similarity: " + nl.similarity(initIdString, fileString) + System.lineSeparator()
     }
-    
-   // resultJsonArr :+ Json.obj("status" ->"OK");
-    
-  
-    
     return Json.toJson(simList.toList)
-    
   }
 }
